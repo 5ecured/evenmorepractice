a902b21e0f44e6410b0e65e0453ece7336ba3017
Edward Ottorino <edward.ottorino@gmail.com>
2020-04-02 11:14:38 +1100

    'commiting'

---
+++ build/precache-manifest.8bfbc1af0fe5869f55dfa8277322c118.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ca8cab3b8ed825393c8bd0fd5167259",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f68d6dad152b3d01d888",
+    "url": "/static/css/main.48a7c636.chunk.css"
+  },
+  {
+    "revision": "e57fc1bf8c80fd6c3b53",
+    "url": "/static/js/2.2ca7c048.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2ca7c048.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f68d6dad152b3d01d888",
+    "url": "/static/js/main.4a6b538b.chunk.js"
+  },
+  {
+    "revision": "fa17551f1be5001209f8",
+    "url": "/static/js/runtime-main.62fbb382.js"
+  }
+]);
